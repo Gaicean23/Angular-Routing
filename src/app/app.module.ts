2f008bb5a5c915b1cba602bb72151fe0b8e36b55
Gaicean23 <gaychanvova60115@mail.ru>
2020-12-29 17:16:25 +0200

    angular-routing

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PhraseListComponent } from './phrase-list/phrase-list.component';
+import { PhraseDetailsComponent } from './phrase-details/phrase-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    PhraseListComponent,
+    PhraseDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/phrase-details/phrase-details.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Phrase } from '../shared/phrase.class';
+import { PhraseService } from '../shared/phrase.service';
+
+@Component({
+  selector: 'app-phrase-details',
+  templateUrl: './phrase-details.component.html',
+  styleUrls: ['./phrase-details.component.scss']
+})
+export class PhraseDetailsComponent implements OnInit {
+
+  phrase: Phrase;
+
+  constructor(private svc: PhraseService, private activatedRoute: ActivatedRoute, private router: Router) { }
+
+  ngOnInit(): void {
+
+    this.activatedRoute.params.forEach((params: Params) => {
+      const id = +params.id;
+
+      this.svc.getPhrase(id).then(res => this.phrase = res);
+    });
+  }
+
+  goToPhrasesList(): void {
+    // /phrases
+    this.router.navigate(['/phrases']);
+  }
+}
