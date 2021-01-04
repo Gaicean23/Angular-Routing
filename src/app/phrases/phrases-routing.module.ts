ee023639fdd858c356a36a03878f6473fd08bde3
Gaicean23 <gaychanvova60115@mail.ru>
2021-01-04 12:58:11 +0200

    добавлена админка, настроены маршруты

--- src/app/phrases/phrases-routing.module.ts
+++ src/app/phrases/phrases-routing.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PhraseDetailsComponent } from './phrase-details/phrase-details.component';
+import { PhraseHomeComponent } from './phrase-home/phrase-home.component';
 import { PhraseListComponent } from './phrase-list/phrase-list.component';
 
 
 const routes: Routes = [
-  { path: 'phrases', component: PhraseListComponent },
-  { path: 'phrases/:id', component: PhraseDetailsComponent }
+  { path: '', redirectTo: '/phrases', pathMatch: 'full' },
+  {
+    path: 'phrases',
+    component: PhraseHomeComponent,
+    children: [
+      {
+        path: '',
+        component: PhraseListComponent,
+        children: [
+          { path: ':id', component: PhraseDetailsComponent },
+          { path: '', component: PhraseDetailsComponent }
+        ]
+      }
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/phrases/phrases.module.ts
+++ src/app/phrases/phrases.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { PhrasesRoutingModule } from './phrases-routing.module';
 import { PhraseListComponent } from './phrase-list/phrase-list.component';
 import { PhraseDetailsComponent } from './phrase-details/phrase-details.component';
+import { PhraseHomeComponent } from './phrase-home/phrase-home.component';
 
 
 
 @NgModule({
   declarations: [
     PhraseListComponent,
-    PhraseDetailsComponent
+    PhraseDetailsComponent,
+    PhraseHomeComponent
   ],
   imports: [
     CommonModule,
